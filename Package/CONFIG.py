a8626f500b1a159793333d2644adf6cfe7d26567
yuanyu.lin <yuanyu.lin@gmail.com>
2017-04-23 12:49:13 +0800

    MAJOR_NUM=1 MINOR_NUM=2 AUX_NUM=0 rework folder structure.

--- Package/CONFIG.py
+++ Package/CONFIG.py
@@ -1,18 +1,41 @@
 import ops
 
-def EXPORT_ENV(args):
-    pkg_path=args["pkg_path"]
-    cc_path=pkg_path + "/gcc-linaro-5.4.1-2017.01-x86_64_arm-linux-gnueabihf/bin"
+def MAIN_ENV(args):
+    pkg_path = args["pkg_path"]
+    output_dir = args["output_path"]
+    cross_compile_tarball = pkg_path + "/gcc-linaro-5.4.1-2017.01-x86_64_arm-linux-gnueabihf.tar.xz"
+    cc_path=output_dir + "/gcc-linaro-5.4.1-2017.01-x86_64_arm-linux-gnueabihf/bin"
     cc_name="arm-linux-gnueabihf-"
 
-    env = {"PATH":cc_path, "CROSS_COMPILE":cc_name}
-    return env
+    env = ops.addEnv("PATH", cc_path)
+    ops.exportEnv(env)
+    env = ops.setEnv("CROSS_COMPILE", cc_name)
+    ops.exportEnv(env)
+    env = ops.setEnv("cross_compiler_tarball", cross_compile_tarball)
+    ops.exportEnv(env)
+    return False
+
+def MAIN_EXTRACT(args):
+    output_dir = args["output_path"]
+    cross_compiler_tarball = ops.getEnv("cross_compiler_tarball")
+    ops.unTarXz(cross_compiler_tarball, output_dir)
+    return True
+
+def MAIN_CONFIGURE(args):
+    output_dir = args["output_path"]
+    return False
+
+def MAIN_BUILD(args):
+    output_dir = args["output_path"]
+    return False
+
+def MAIN_INSTALL(args):
+    output_dir = args["output_path"]
+    return False
+
+def MAIN_CLEAN_BUILD(args):
+    output_dir = args["output_path"]
+    return False
 
 def MAIN(args):
-    output_path=ops.pkg_mkdir(args["pkg_path"], "debian_jessie_armhf")
-    qemu_path=ops.pkg_mkdir(output_path, "/usr/bin")
-    print qemu_path
-    ops.copyto("/usr/bin/qemu-arm-static", output_path + "/usr/bin/qemu-arm-static")
-    arch="armhf"
-    CMD=["debootstrap", "--arch=" + arch, "--variant=minbase", "jessie", output_path]
-    res = ops.execCmd(CMD, output_path, False, None)
+    print "cross_compile"
