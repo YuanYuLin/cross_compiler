abdd419f44835be580d71e0f845712b2b34d79d7
yuanyu.lin <yuanyu.lin@gmail.com>
2017-07-03 10:46:57 +0800

    MAJOR_NUM=1 MINOR_NUM=4 AUX_NUM=0 add crosscompilers 1. CodeSocery GCC - arm-2014.05-29-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2 2. Linaro GCC     - gcc-linaro-5.4.1-2017.01-x86_64_arm-linux-gnueabi.tar.xz 3. Buildroot GCC  - iopc_gcc_armel_2017.02.3.tar.xz

--- Package/CONFIG.py
+++ Package/CONFIG.py
@@ -1,64 +1,160 @@
 import ops
 import iopc
 
-def MAIN_ENV(args):
+pkg_path = ""
+output_dir = ""
+arch = ""
+arch_cc_version = ""
+cross_compiler_tarball = ""
+cross_compiler_tarball_type = ""
+cross_compiler_tarball_root = ""
+cc_path = ""
+cc_name = ""
+cc_sysroot_lib = ""
+cc_version = ""
+
+def set_global(args):
+    global pkg_path
+    global output_dir
+    global arch
+    global arch_cc_version
+    global cross_compiler_tarball
+    global cross_compiler_tarball_type
+    global cross_compiler_tarball_root
+    global cc_path
+    global cc_name
+    global cc_sysroot_lib
+    global cc_version
     pkg_path = args["pkg_path"]
     output_dir = args["output_path"]
-    cross_compile_tarball = pkg_path + "/gcc-linaro-5.4.1-2017.01-x86_64_arm-linux-gnueabihf.tar.xz"
-    cc_path=output_dir + "/gcc-linaro-5.4.1-2017.01-x86_64_arm-linux-gnueabihf/bin"
-    cc_name="arm-linux-gnueabihf-"
+    arch = ops.getEnv("ARCH_ALT")
+    pkg_args = args["pkg_args"]
+    arch_cc_version = pkg_args["version"]
+    if arch == "armhf":
+        cross_compiler_tarball = ops.path_join(pkg_path, "gcc-linaro-5.4.1-2017.01-x86_64_arm-linux-gnueabihf.tar.xz")
+        cross_compiler_tarball_type = "XZ"
+        cross_compiler_tarball_root = output_dir
+        cc_path=ops.path_join(output_dir, "gcc-linaro-5.4.1-2017.01-x86_64_arm-linux-gnueabihf/bin")
+        cc_name=ops.path_join(cc_path, "arm-linux-gnueabihf-")
+        cc_sysroot_lib = ops.path_join(output_dir, "gcc-linaro-5.4.1-2017.01-x86_64_arm-linux-gnueabi/arm-linux-gnueabi/libc/lib")
+        cc_version = "2.21"
+    elif arch == "armel":
+        if arch_cc_version == "Linaro-2017.01":
+            cross_compiler_tarball = ops.path_join(pkg_path, "gcc-linaro-5.4.1-2017.01-x86_64_arm-linux-gnueabi.tar.xz")
+            cross_compiler_tarball_type = "XZ"
+            cross_compiler_tarball_root = output_dir
+            cc_path=ops.path_join(output_dir, "gcc-linaro-5.4.1-2017.01-x86_64_arm-linux-gnueabi/bin")
+            cc_name=ops.path_join(cc_path, "arm-linux-gnueabi-")
+            cc_sysroot_lib = ops.path_join(output_dir, "gcc-linaro-5.4.1-2017.01-x86_64_arm-linux-gnueabi/arm-linux-gnueabi/libc/lib")
+            cc_version = "2.21"
+        elif arch_cc_version == "CodeSourcery-2014.05":
+            cross_compiler_tarball = ops.path_join(pkg_path, "arm-2014.05-29-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2")
+            cross_compiler_tarball_type = "BZ2"
+            cross_compiler_tarball_root = output_dir
+            cc_path=ops.path_join(output_dir, "arm-2014.05/bin")
+            cc_name=ops.path_join(cc_path, "arm-none-linux-gnueabi-")
+            cc_sysroot_lib = ops.path_join(output_dir, "arm-2014.05/arm-none-linux-gnueabi/libc/armv4t/lib")
+            cc_version = "2.18"
+        elif arch_cc_version == "iopc_gcc_armel_2017.02.3":
+            cross_compiler_tarball = ops.path_join(pkg_path, "iopc_gcc_armel_2017.02.3.tar.xz")
+            cross_compiler_tarball_type = "XZ"
+            cross_compiler_tarball_root = "/opt"
+            cc_path=ops.path_join(cross_compiler_tarball_root, "iopc_gcc_armel_2017.02.3/usr/bin")
+            cc_name=ops.path_join(cc_path, "arm-iopc-linux-gnueabi-")
+            cc_sysroot_lib = ops.path_join(cross_compiler_tarball_root, "iopc_gcc_armel_2017.02.3/usr/arm-iopc-linux-gnueabi/sysroot/lib")
+            cc_version = "2.24"
+        else:
+            sys.exit(1)
+    else:
+        sys.exit(1)
 
-    env = ops.addEnv("PATH", cc_path)
-    ops.exportEnv(env)
-    env = ops.setEnv("CROSS_COMPILE", cc_name)
-    ops.exportEnv(env)
-    env = ops.setEnv("cross_compiler_tarball", cross_compile_tarball)
-    ops.exportEnv(env)
+def MAIN_ENV(args):
+    set_global(args)
+
+    ops.exportEnv(ops.addEnv("PATH", cc_path))
+    ops.exportEnv(ops.setEnv("CROSS_COMPILE", cc_name))
     return False
 
 def MAIN_EXTRACT(args):
-    output_dir = args["output_path"]
-    cross_compiler_tarball = ops.getEnv("cross_compiler_tarball")
-    ops.unTarXz(cross_compiler_tarball, output_dir)
+    set_global(args)
+
+    if cross_compiler_tarball_root == "/opt":
+        if ops.isExist(ops.path_join(cross_compiler_tarball_root, arch_cc_version)):
+            return False
+
+    if cross_compiler_tarball_type == "XZ":
+        ops.unTarXz(cross_compiler_tarball, cross_compiler_tarball_root)
+    elif cross_compiler_tarball_type == "BZ2":
+        ops.unTarBz2(cross_compiler_tarball, cross_compiler_tarball_root)
+    elif cross_compiler_tarball_type == "GZ":
+        ops.unTarGz(cross_compiler_tarball, cross_compiler_tarball_root)
+    else:
+        sys.exit(1)
+
+    if cross_compiler_tarball_root == "/opt":
+        ops.touch(ops.path_join(cross_compiler_tarball_root, arch_cc_version))
+
+    return True
+
+def MAIN_PATCH(args, patch_group_name):
+    set_global(args)
+    for patch in iopc.get_patch_list(pkg_path, patch_group_name):
+        if iopc.apply_patch(output_dir, patch):
+            continue
+        else:
+            sys.exit(1)
+
     return True
 
 def MAIN_CONFIGURE(args):
-    output_dir = args["output_path"]
+    set_global(args)
+
     return False
 
 def MAIN_BUILD(args):
-    output_dir = args["output_path"]
+    set_global(args)
+
     return False
 
 def MAIN_INSTALL(args):
-    output_dir = args["output_path"]
-    cc_lib_path = ops.path_join(output_dir, "gcc-linaro-5.4.1-2017.01-x86_64_arm-linux-gnueabihf/arm-linux-gnueabihf/libc/lib")
-    iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "ld-2.21.so"), "lib")
-    iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "ld-linux-armhf.so.3"), "lib")
+    set_global(args)
+    cc_lib_path = cc_sysroot_lib
+ 
+    if arch == "armhf":
+        iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "ld-linux-armhf.so.3"), "lib")
 
-    iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "libc-2.21.so"), "lib")
+    if arch == "armel":
+        iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "ld-linux.so.3"), "lib")
+
+    iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "libgcc_s.so.1"), "lib")
+    iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "libgcc_s.so"), "lib")
+
+    iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "ld-{}.so".format(cc_version)), "lib")
+
+    iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "libc-{}.so".format(cc_version)), "lib")
     iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "libc.so.6"), "lib")
 
-    iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "librt-2.21.so"), "lib")
+    iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "librt-{}.so".format(cc_version)), "lib")
     iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "librt.so.1"), "lib")
 
-    iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "libpthread-2.21.so"), "lib")
+    iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "libpthread-{}.so".format(cc_version)), "lib")
     iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "libpthread.so.0"), "lib")
 
-    iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "libdl-2.21.so"), "lib")
+    iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "libdl-{}.so".format(cc_version)), "lib")
     iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "libdl.so.2"), "lib")
 
-    iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "libm-2.21.so"), "lib")
+    iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "libm-{}.so".format(cc_version)), "lib")
     iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "libm.so.6"), "lib")
 
-    iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "libcrypt-2.21.so"), "lib")
+    iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "libcrypt-{}.so".format(cc_version)), "lib")
     iopc.installBin(args["pkg_name"], ops.path_join(cc_lib_path, "libcrypt.so.1"), "lib")
 
     return False
 
 def MAIN_CLEAN_BUILD(args):
-    output_dir = args["output_path"]
+    set_global(args)
     return False
 
 def MAIN(args):
-    print "cross_compile"
+    set_global(args)
+    print "cross_compiler"
